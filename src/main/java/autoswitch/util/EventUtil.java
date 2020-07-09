c29b7601b153aaeb33d4beeecd9e5c1f93d7e42f
Deximus-Maximus <kev12east@gmail.com>
2020-07-08 20:09:54 -0400

    init event system implementation

---
+++ src/main/java/autoswitch/util/EventUtil.java
@@ -0,0 +1,20 @@
+package autoswitch.util;
+
+import autoswitch.AutoSwitch;
+import autoswitch.events.SwitchEvent;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+
+public class EventUtil {
+
+    public static ActionResult eventHandler(World world, int currentTime, double deltaTime, SwitchEvent event) {
+
+        if (!world.isClient()) return ActionResult.PASS; // Make sure this is only run on client
+
+        if (AutoSwitch.data.getHasSwitched()) deltaTime += AutoSwitch.switchDelay; //todo fix this being fired twice as tool switching triggers it a second time
+
+        AutoSwitch.scheduler.schedule(event.setWorld(true), deltaTime, currentTime);
+
+        return ActionResult.PASS;
+    }
+}
