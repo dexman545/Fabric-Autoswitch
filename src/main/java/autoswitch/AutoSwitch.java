38e0472a6aa8cd36b71256c3038e20beda7e5820
Deximus-Maximus <kev12east@gmail.com>
2020-07-15 21:36:36 -0400

    Fix switchback not respecting delays

--- src/main/java/autoswitch/AutoSwitch.java
+++ src/main/java/autoswitch/AutoSwitch.java
@@ -113,7 +113,7 @@ public class AutoSwitch implements ClientModInitializer {
 
             //Checks for implementing switchback feature
             if (e.player != null) {
-                SwitchEvent.SWITCHBACK.setPlayer(e.player).invoke();
+                //SwitchEvent.SWITCHBACK.setPlayer(e.player).invoke();
                 EventUtil.eventHandler(e.world, tickTime, 0, SwitchEvent.SWITCHBACK.setPlayer(e.player));
                 //SwitchEvent.HAS_SWITCHED.invoke();
                 //scheduler.schedule(SwitchEvent.SWITCHBACK.setPlayer(e.player), 10, tickTime); //schedules it every tick to offset is meaningless
--- src/main/java/autoswitch/util/EventUtil.java
+++ src/main/java/autoswitch/util/EventUtil.java
@@ -7,12 +7,29 @@ import net.minecraft.world.World;
 
 public class EventUtil {
 
+    private static boolean hasScheduledSwitchback = false;
+
     public static ActionResult eventHandler(World world, int currentTime, double deltaTime, SwitchEvent event) {
 
         if (!world.isClient()) return ActionResult.PASS; // Make sure this is only run on client
 
         if (AutoSwitch.data.getHasSwitched()) deltaTime += AutoSwitch.switchDelay;
 
+        //Fix switchback not being delayed
+        if (event == SwitchEvent.SWITCHBACK) {
+
+            // TODO improve so special case of switchback isn't needed
+            if (AutoSwitch.data.getHasSwitched() && !SwitchEvent.player.handSwinging && !hasScheduledSwitchback) {
+                AutoSwitch.scheduler.schedule(event.setWorld(true), 6, currentTime);
+                hasScheduledSwitchback = true;
+            }
+
+            return ActionResult.PASS;
+        }
+
+        hasScheduledSwitchback = false;
+
+        // Normal handing of switches
         AutoSwitch.scheduler.schedule(event.setWorld(true), deltaTime, currentTime);
 
         return ActionResult.PASS;
