3cddd1a60839aad6fd5118f3ef25a1d658df82d1
Deximus-Maximus <kev12east@gmail.com>
2020-09-04 02:56:08 -0400

    Add JD to TargetableCache

--- src/main/java/autoswitch/util/TargetableCache.java
+++ src/main/java/autoswitch/util/TargetableCache.java
@@ -1,8 +1,13 @@
 package autoswitch.util;
 
-import it.unimi.dsi.fastutil.objects.Object2IntLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Reference2IntLinkedOpenHashMap;
 
+/**
+ * With the new way of triggering switches (not FAPI), the eval. ran constantly when the keys were held.
+ * This led to a lot of lag. The cache was added to save recalculating switches.
+ * It is designed to have a maximum size, discarding the oldest added element, and to have entries removed based on
+ * the slot it was mapping to, allowing for more targeted recalculation.
+ */
 public class TargetableCache extends Reference2IntLinkedOpenHashMap<Object> {
 
     private final int maxSize;
@@ -21,7 +26,7 @@ public class TargetableCache extends Reference2IntLinkedOpenHashMap<Object> {
     public void removePairOnValue(final int v) {
         final int[] value = this.value;
         final Object[] key = this.key;
-        for (int i = n; i-- != 0;)
+        for (int i = n; i-- != 0; )
             if (!((key[i]) == null) && ((value[i]) == (v)))
                 this.remove(key[i], v);
     }
