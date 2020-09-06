419814d2b216ac073c6da3a09115c5cef5cd163e
Deximus-Maximus <kev12east@gmail.com>
2020-09-05 22:06:59 -0400

    Generify CaseInsensitiveEnumConverter

--- src/main/java/autoswitch/config/util/CaseInsensitiveEnumConverter.java
+++ src/main/java/autoswitch/config/util/CaseInsensitiveEnumConverter.java
@@ -1,24 +1,40 @@
 package autoswitch.config.util;
 
-import autoswitch.config.AutoSwitchConfig;
+import autoswitch.AutoSwitch;
 import org.aeonbits.owner.Converter;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Method;
+import java.util.Locale;
 
-public class CaseInsensitiveEnumConverter implements Converter<AutoSwitchConfig.SwitchDelay> {
+public class CaseInsensitiveEnumConverter<X extends Enum<X>> implements Converter<X> {
+
     @Override
-    public AutoSwitchConfig.SwitchDelay convert(Method method, String input) {
+    @SuppressWarnings("unchecked")
+    public X convert(Method method, String input) {
 
-        switch (input.toLowerCase()) {
-            case "blocks":
-                return AutoSwitchConfig.SwitchDelay.BLOCKS;
-            case "mobs":
-                return AutoSwitchConfig.SwitchDelay.MOBS;
-            case "both":
-                return AutoSwitchConfig.SwitchDelay.BOTH;
-            default:
-                return AutoSwitchConfig.SwitchDelay.NONE;
+        Class<X> t = (Class<X>) method.getReturnType();
+        X e;
+        if ((e = searchEnum(t, input.toUpperCase(Locale.ENGLISH))) == null) {
+            AutoSwitch.logger.error("Could not parse value: {} on {}; Defaulting to another.",
+                    input, method.getName());
+            return t.getEnumConstants()[0];
         }
+        return e;
 
+    }
+
+
+    /**
+     * Could be replaced with EnumUtils#getEnum()
+     * @author https://stackoverflow.com/a/28333189
+     */
+    private static <T extends Enum<?>> @Nullable T searchEnum(Class<T> enumeration, String search) {
+        for (T each : enumeration.getEnumConstants()) {
+            if (each.name().compareToIgnoreCase(search) == 0) {
+                return each;
+            }
+        }
+        return null;
     }
 }
