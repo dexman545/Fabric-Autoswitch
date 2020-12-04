50e55bf073f686c62505166b786302b98dd1edb8
Deximus-Maximus <kev12east@gmail.com>
2020-12-04 07:18:53 -0500

    New switch trigger

---
+++ src/main/java/autoswitch/mixin/mixins/MixinClientPlayerInteractionManager.java
@@ -0,0 +1,33 @@
+package autoswitch.mixin.mixins;
+
+import autoswitch.mixin.impl.SwitchEventTriggerImpl;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public class MixinClientPlayerInteractionManager {
+
+    @Shadow @Final private MinecraftClient client;
+
+    /**
+     * Trigger the switch events.
+     */
+    @Inject(at = @At("HEAD"), method = "syncSelectedSlot()V")
+    private void triggerSwitchOnSlotSync(CallbackInfo ci) {
+        assert this.client.player != null;
+        if (this.client.options.keyAttack.isPressed() || this.client.options.keyAttack.wasPressed()) {
+            SwitchEventTriggerImpl.attack(0,
+                    this.client.player, this.client.world, this.client.crosshairTarget);
+        } else if (this.client.options.keyUse.isPressed()) {
+            SwitchEventTriggerImpl.interact((ClientPlayerInteractionManager)(Object) this,
+                    this.client.player, this.client.world, this.client.crosshairTarget);
+        }
+    }
+
+}
--- src/main/java/autoswitch/mixin/mixins/MixinMinecraftClient.java
+++ src/main/java/autoswitch/mixin/mixins/MixinMinecraftClient.java
@@ -22,75 +22,12 @@ public abstract class MixinMinecraftClient {
 
     @Shadow
     @Nullable
-    public HitResult crosshairTarget;
-
-    @Shadow
-    @Nullable
     public ClientPlayerEntity player;
-
-    @Shadow
-    @Nullable
-    public ClientWorld world;
-
-    @Shadow
-    @Nullable
-    public ClientPlayerInteractionManager interactionManager;
-    @Shadow
-    protected int attackCooldown;
-    @Unique
-    private Vec3d target;
-
-    /**
-     * Trigger for ATTACK event.
-     */
-    @Inject(at = @At("INVOKE"), method = "doAttack")
-    private void attackEvent(CallbackInfo ci) {
-        assert this.player != null;
-
-        SwitchEventTriggerImpl.attack(this.attackCooldown, this.player, this.world, this.crosshairTarget);
-
-    }
-
-    /**
-     * Trigger for USE event.
-     */
-    @Inject(at = @At("INVOKE"), method = "doItemUse")
-    private void useEvent(CallbackInfo ci) {
-        assert this.player != null;
-        assert this.interactionManager != null;
-
-        if (this.target == (this.crosshairTarget != null ? this.crosshairTarget.getPos() : null)) return;
-
-        this.target = this.crosshairTarget.getPos();
-
-        SwitchEventTriggerImpl.interact(this.interactionManager, this.player, this.world, this.crosshairTarget);
-
-    }
-
-    /**
-     * Fix for doAttack only being called once the key is first pressed, this fixes not switching tools when moving
-     * to a new block from the previous one, such as when the first block is broken.
-     */
-    @Inject(at = @At("INVOKE"), method = "handleBlockBreaking")
-    private void blockAttackEventSecondary(boolean bl, CallbackInfo ci) {
-        //todo see if a secondary event is needed for entities, targetedEntity and tick seem like good places to use
-        //todo see if primary attack event should be removed
-        if (!bl || this.crosshairTarget == null || this.crosshairTarget.getType() != HitResult.Type.BLOCK) return;
-
-        if (this.crosshairTarget.getPos().equals(this.target)) return;
-
-        this.target = this.crosshairTarget.getPos();
-
-        assert this.player != null;
-
-        SwitchEventTriggerImpl.attack(this.attackCooldown, this.player, this.world, this.crosshairTarget);
-
-    }
 
     /**
      * Reset keybinding params and switch state when leaving a world.
      */
-    @Inject(at = @At("INVOKE"), method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V")
+    @Inject(at = @At("HEAD"), method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;)V")
     private void disconnectEvent(Screen screen, CallbackInfo ci) {
         DisconnectHandler.reset();
 
