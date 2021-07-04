89ce41e39d0b0d31a01b054e5986fe21c7cabf47
Deximus-Maximus <kev12east@gmail.com>
2021-07-03 21:39:45 -0400

    Document PlayerEntityAccessor

--- src/main/java/autoswitch/mixin/mixins/PlayerEntityAccessor.java
+++ src/main/java/autoswitch/mixin/mixins/PlayerEntityAccessor.java
@@ -6,6 +6,11 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 
+/**
+ * In 1.17, {@link PlayerEntity#inventory} was made private, with usage replaced by {@link PlayerEntity#getInventory()}.
+ * This accessor should use that method in 1.17+, while still allowing AutoSwitch to function in 1.16.
+ */
+@SuppressWarnings("JavadocReference")
 @Mixin(PlayerEntity.class)
 public interface PlayerEntityAccessor {
     @Accessor
