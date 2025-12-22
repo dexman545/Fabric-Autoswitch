a297483a3032cd630c0c6886360960741a298582
Deximus-Maximus <kev12east@gmail.com>
2025-12-22 03:40:22 -0500

    Add support for attachments to SelectionContext to pass more data during selection
    (cherry picked from commit 377a4fa98bdde8914f814651b72ebed530b7e539)

--- engine/src/main/java/dex/autoswitch/engine/data/SelectionContext.java
+++ engine/src/main/java/dex/autoswitch/engine/data/SelectionContext.java
@@ -1,6 +1,40 @@
 package dex.autoswitch.engine.data;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import dex.autoswitch.engine.Action;
 
-public record SelectionContext(Action action, Object target) {
+public record SelectionContext(
+    Action action, 
+    Object target, 
+    Map<ContextKey<?>, Object> attachments
+) {
+    @SafeVarargs
+    public SelectionContext(Action action, Object target, Map.Entry<ContextKey<?>, Object>... attachmentEntries) {
+        this(action, target, Map.ofEntries(attachmentEntries));
+    }
+
+    public <T> Optional<T> getOptional(ContextKey<T> key) {
+        return Optional.ofNullable(get(key));
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(ContextKey<T> key) {
+        return (T) attachments.get(key);
+    }
+
+    public SelectionContext with(Map<ContextKey<?>, Object> extraData) {
+        var newAttachments = new HashMap<>(attachments);
+        newAttachments.putAll(extraData);
+        return new SelectionContext(action, target, Collections.unmodifiableMap(newAttachments));
+    }
+
+    public <T> SelectionContext with(ContextKey<T> key, T value) {
+        var newAttachments = new HashMap<>(attachments);
+        newAttachments.put(key, value);
+        return new SelectionContext(action, target, Map.copyOf(newAttachments));
+    }
 }
--- engine/src/main/java/dex/autoswitch/engine/state/SwitchContext.java
+++ engine/src/main/java/dex/autoswitch/engine/state/SwitchContext.java
@@ -1,17 +1,31 @@
 package dex.autoswitch.engine.state;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import dex.autoswitch.config.AutoSwitchConfig;
 import dex.autoswitch.engine.Action;
+import dex.autoswitch.engine.data.ContextKey;
+import dex.autoswitch.engine.data.SelectionContext;
 import dex.autoswitch.engine.data.extensible.PlayerInventory;
 import dex.autoswitch.engine.events.Scheduler;
 
-import java.util.Optional;
-
 public record SwitchContext(PlayerInventory<?> player, AutoSwitchConfig config,
                             Action action, Object target,
-                            SwitchState switchState, Scheduler scheduler) {
+                            SwitchState switchState, Scheduler scheduler,
+                            Map<ContextKey<?>, Object> attachments) {
+
+    @SafeVarargs
+    public SwitchContext(PlayerInventory<?> player, AutoSwitchConfig config,
+                         Action action, Object target,
+                         SwitchState switchState, Scheduler scheduler, Map.Entry<ContextKey<?>, Object>... attachmentEntries) {
+        this(player, config, action, target, switchState, scheduler, Map.ofEntries(attachmentEntries));
+    }
+
     public Optional<Boolean> findSlot() {
-        var maybeSlot = config.getEngine().findSlot(player, action, target);
+        var maybeSlot = config.getEngine().findSlot(player, new SelectionContext(action, target, attachments));
 
         if (maybeSlot.isPresent()) {
             return Optional.of(switchSlot(maybeSlot.getAsInt()));
@@ -28,6 +42,12 @@ public record SwitchContext(PlayerInventory<?> player, AutoSwitchConfig config,
     }
 
     public SwitchContext withTarget(Object o) {
-        return new SwitchContext(player, config, action, o, switchState, scheduler);
+        return new SwitchContext(player, config, action, o, switchState, scheduler, attachments);
+    }
+
+    public <T> SwitchContext with(ContextKey<T> key, T value) {
+        var newAttachments = new HashMap<>(attachments);
+        newAttachments.put(key, value);
+        return new SwitchContext(player, config, action, target, switchState, scheduler, Collections.unmodifiableMap(newAttachments));
     }
 }
