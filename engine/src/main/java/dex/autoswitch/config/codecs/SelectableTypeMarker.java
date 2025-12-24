bd8043bb2fc09e31700ead8e21d5d03897ec9199
Deximus-Maximus <kev12east@gmail.com>
2025-12-24 09:22:54 -0500

    Allow eliding type for offhand tool selectors

--- engine/src/main/java/dex/autoswitch/config/codecs/SelectableTypeMarker.java
+++ engine/src/main/java/dex/autoswitch/config/codecs/SelectableTypeMarker.java
@@ -7,6 +7,10 @@ import java.lang.annotation.Target;
 
 import org.jspecify.annotations.NonNull;
 
+/**
+ * A marker annotation for to provide the type so that users may elide the {@code type=<value>} from
+ * {@link dex.autoswitch.config.data.tree.IdSelector} definitions in a config file in certain contexts.
+ */
 @Target(ElementType.TYPE_USE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SelectableTypeMarker {
