a65d05a568dfdaba6add06b0678bbfe8a9000205
Deximus-Maximus <kev12east@gmail.com>
2025-12-24 23:44:07 -0500

    Add some javadoc

--- engine/src/main/java/dex/autoswitch/engine/Selector.java
+++ engine/src/main/java/dex/autoswitch/engine/Selector.java
@@ -1,11 +1,13 @@
 package dex.autoswitch.engine;
 
+import java.util.Objects;
+
 import dex.autoswitch.engine.data.Match;
 import dex.autoswitch.engine.data.SelectionContext;
 
-import java.util.Objects;
-
 /**
+ * A wrapper around a {@link Matcher} that associates it with a given {@code priority} and a base level of {@code 0} to
+ * begin the matching process.
  * @param priority the priority of this {@code Selector} in relation to other {@code Selector}s of the same type
  */
 public record Selector(int priority, Matcher matcher) {
