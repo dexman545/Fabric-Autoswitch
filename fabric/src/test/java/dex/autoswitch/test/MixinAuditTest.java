4e35194c650a8ed469a17cd83b5a35315a0fb442
Deximus-Maximus <kev12east@gmail.com>
2026-01-13 11:46:25 -0500

    Add mixin audits

---
+++ fabric/src/test/java/dex/autoswitch/test/MixinAuditTest.java
@@ -0,0 +1,21 @@
+package dex.autoswitch.test;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.server.Bootstrap;
+
+public class MixinAuditTest {
+    @BeforeAll
+    static void beforeAll() {
+        SharedConstants.tryDetectVersion();
+        Bootstrap.bootStrap();
+    }
+
+    @Test
+    void auditMixins() {
+        MixinEnvironment.getCurrentEnvironment().audit();
+    }
+}
