1a18c8440eddf464c53a71467475f74482654bd5
Deximus-Maximus <kev12east@gmail.com>
2026-01-17 16:23:06 -0500

    Support client tags on neoforge

--- neoforge/src/main/java/dex/autoswitch/platform/NeoForgePlatformHelper.java
+++ neoforge/src/main/java/dex/autoswitch/platform/NeoForgePlatformHelper.java
@@ -1,22 +1,16 @@
 package dex.autoswitch.platform;
 
 import java.nio.file.Path;
-import java.util.Objects;
-import java.util.Optional;
 
 import com.google.auto.service.AutoService;
+import dex.autoswitch.fapi.client.api.ClientTags;
 import dex.autoswitch.platform.services.IPlatformHelper;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.common.CommonHooks;
 
-import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
@@ -46,19 +40,7 @@ public class NeoForgePlatformHelper implements IPlatformHelper {
 
     @Override
     public <T> boolean isInTagGeneral(TagKey<T> tagKey, T t) {
-        // Neo does not have client tags, so use MC normally
-        // BlockState#is(TagKey) and such exists, but without forwarding the original type here we can't use it
-        var reg = getRegistry(getRegistryAccess(), tagKey);
-        if (reg.isPresent()) {
-            if (reg.get().get(tagKey).isPresent()) {
-                var entry = getRegistryEntry(reg.get(), tagKey, t);
-                if (entry.isPresent()) {
-                    return entry.get().is(tagKey);
-                }
-            }
-        }
-
-        return false;
+        return ClientTags.isInWithLocalFallback(tagKey, t);
     }
 
     @Override
@@ -69,27 +51,5 @@ public class NeoForgePlatformHelper implements IPlatformHelper {
         }
 
         return stack.getTagEnchantments();
-    }
-
-    @SuppressWarnings("unchecked")
-    private static <T> Optional<? extends Registry<T>> getRegistry(RegistryAccess registryAccess, TagKey<T> tagKey) {
-        Objects.requireNonNull(tagKey);
-
-        // Check if the tag represents a dynamic registry
-        var maybeRegistry = registryAccess.lookup(tagKey.registry());
-        if (maybeRegistry.isPresent()) return maybeRegistry;
-
-        // Return the default registries
-        return (Optional<? extends Registry<T>>) BuiltInRegistries.REGISTRY.getOptional(tagKey.registry().location());
-    }
-
-    private static <U extends Registry<T>, T> Optional<Holder<T>> getRegistryEntry(U registry, TagKey<T> tagKey, T entry) {
-        if (!tagKey.isFor(registry.key())) {
-            return Optional.empty();
-        }
-
-        Optional<ResourceKey<T>> maybeKey = registry.getResourceKey(entry);
-
-        return maybeKey.map(registry::getOrThrow);
     }
 }
